0de8972a6cddcb28b4a1234418e20d4496fd4311
nutchawat <pnutchaw@gmail.com>
2017-09-17 13:53:13 +0700

    change email p.warach@gmail.com to info@vvpfoto.com

--- src/contact/api/mocker.js
+++ src/contact/api/mocker.js
@@ -2,8 +2,8 @@ export const mocker = {
   contact () {
     return {
       head: 'CONTACT',
-      email: 'p.warach@gmail.com',
-      // email: 'info@vvpfoto.com',
+      // email: 'p.warach@gmail.com',
+      email: 'info@vvpfoto.com',
       tel1: '+81 80 9390 2525',
       tel1_desc: '(Japan)',
       tel2: '+66 86 888 2237',
