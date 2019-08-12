6ce9737df0592a8f1e93453a1236a1fc50222ff4
nutchawat <pnutchaw@gmail.com>
2019-08-13 00:08:45 +0700

    update workI

--- src/workI/components/index.vue
+++ src/workI/components/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div name="workI">
-    <underconstruction />
-<!--     <vue-perfect-layout v-show="indexDisp"
+    <!--<underconstruction />-->
+    <vue-perfect-layout v-show="indexDisp"
                         :photos="workI"
                         :togglePage="togglePage"
                         :receivedDetail="receivedWorkIDetail"
@@ -9,39 +9,39 @@
     <vue-light-gallery  v-show="detailDisp"
                         :photos="workIDetail"
                         :togglePage="togglePage"
-    /> -->
+    />
   </div>
 </template>
 
 <script>
-  import underconstruction from 'src/underconstruction/components'
-  // import { mapGetters, mapMutations, mapActions } from 'vuex'
-  // import * as types from 'src/workI/store/mutation-types'
+  // import underconstruction from 'src/underconstruction/components'
+  import { mapGetters, mapMutations, mapActions } from 'vuex'
+  import * as types from 'src/workI/store/mutation-types'
 
   export default {
     name: 'workI',
-    components: {
-      underconstruction
-    }
-    // created () {
-    //   this.receivedWorkI()
-    // },
-    // computed: {
-    //   ...mapGetters({
-    //     indexDisp: types.GET_WORKI_INDEX_DISP,
-    //     detailDisp: types.GET_WORKI_DETAIL_DISP,
-    //     workI: types.GET_WORKI,
-    //     workIDetail: types.GET_WORKI_DETAIL
-    //   })
-    // },
-    // methods: {
-    //   ...mapMutations({
-    //     togglePage: types.SET_WORKI_TOGGLE_PAGE
-    //   }),
-    //   ...mapActions({
-    //     receivedWorkI: types.RECEIVED_WORKI,
-    //     receivedWorkIDetail: types.RECEIVED_WORKI_DETAIL
-    //   })
+    // components: {
+    //   underconstruction
     // }
+    created () {
+      this.receivedWorkI()
+    },
+    computed: {
+      ...mapGetters({
+        indexDisp: types.GET_WORKI_INDEX_DISP,
+        detailDisp: types.GET_WORKI_DETAIL_DISP,
+        workI: types.GET_WORKI,
+        workIDetail: types.GET_WORKI_DETAIL
+      })
+    },
+    methods: {
+      ...mapMutations({
+        togglePage: types.SET_WORKI_TOGGLE_PAGE
+      }),
+      ...mapActions({
+        receivedWorkI: types.RECEIVED_WORKI,
+        receivedWorkIDetail: types.RECEIVED_WORKI_DETAIL
+      })
+    }
   }
 </script>
