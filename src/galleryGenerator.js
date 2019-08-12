cd0959b0988c0a54fe3bd18b60d8af3869f33d15
nutchawat <pnutchaw@gmail.com>
2019-08-13 00:45:42 +0700

    change ratio W, W2, H, H2 has same scale 279:365

--- src/galleryGenerator.js
+++ src/galleryGenerator.js
@@ -13,15 +13,15 @@ module.exports = {
         if (typeof image.isSelect !== 'undefined') {
           if (image.isSelect) {
             if (image.display === 'W') {
-              ratio = 1.5047879616963065
+              ratio = 1.3082437276
             } else if (image.display === 'W2') {
-              ratio = 1.8
+              ratio = 1.3082437276
             } else if (image.display === 'H') {
-              ratio = 0.6645454545454546
+              ratio = 0.76438356164
             } else if (image.display === 'H2') {
-              ratio = 1.0
+              ratio = 0.76438356164
             } else {
-              ratio = 0.6645454545454546
+              ratio = 0.76438356164
             }
             /*
             let img = document.createElement('img')
@@ -47,15 +47,15 @@ module.exports = {
       if (subFolder.folderName === folderName) {
         for (let image of subFolder.images) {
           if (image.display === 'W') {
-            ratio = 1.5047879616963065
+            ratio = 1.3082437276
           } else if (image.display === 'W2') {
-            ratio = 1.8
+            ratio = 1.3082437276
           } else if (image.display === 'H') {
-            ratio = 0.6645454545454546
+            ratio = 0.76438356164
           } else if (image.display === 'H2') {
-            ratio = 1.0
+            ratio = 0.76438356164
           } else {
-            ratio = 0.6645454545454546
+            ratio = 0.76438356164
           }
           mockerDetail.push({
             src: baseImgPath + mockerJson.folderName + '/' + folderName + '/' + image.name,
